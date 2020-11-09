5a76caacbf8bf93cee95cab6ccac5cc25d5efba9
Israel Said Martinez Lopez <73085642+IsraelSaid@users.noreply.github.com>
2020-11-08 22:21:00 -0600

    Cambios de nombre a variables

--- WebPageCode/add-product.php
+++ WebPageCode/add-product.php
@@ -115,7 +115,7 @@
           <div class="tm-bg-primary-dark tm-block tm-block-h-auto">
             <div class="row">
               <div class="col-12">
-                <h2 class="tm-block-title d-inline-block">Add Product</h2>
+                <h2 class="tm-block-title d-inline-block">Agregar nuevo producto</h2>
               </div>
             </div>
             <div class="row tm-edit-product-row">
@@ -124,7 +124,7 @@
                   <div class="form-group mb-3">
                     <label
                       for="name"
-                      >Product Name
+                      >Nombre del producto
                     </label>
                     <input
                       id="name"
